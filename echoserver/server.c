e5a4b295e2af301236834dd6352757c8a077102d
ahamednadeem <ahamed@mobicip.com>
2024-02-26 14:59:51 +0530

    Echo server

--- echoserver/server.c
+++ echoserver/server.c
@@ -75,7 +75,8 @@ int main()
         strcat(buffer, " ");
       	strcat(buffer, timebuffer);
       	int a;
-    	scanf("%d",&a);
+    	scanf("%d",&a); 
+    	
         printf("Now echoing Back to the server\n");
         send(client_socket, buffer, bytes_received + 25, 0);  // Echo back the received data
     }
